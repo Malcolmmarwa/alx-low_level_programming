476ef2fd7162982b46d75a84ad0a4b099f38af44
Malcolmmarwa <88811859+Malcolmmarwa@users.noreply.github.com>
2023-09-05 12:43:21 +0300

    Edited the function to Binary C conversion.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,27 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: Pointer to a string containing the binary number.
- *
- * Return: The converted number, or 0 if the input is not a valid binary string.
- */
-unsigned int binary_to_uint(const char *b)
-{
-    unsigned int result = 0;
-
+unsigned int binary_to_uint(const char *b) {
     if (b == NULL)
         return 0;
 
-    for (; *b != '\0'; b++)
-    {
-        if (*b != '0' && *b != '1')
+    unsigned int result = 0;
+    int i = 0;
+
+    while (b[i] != '\0') {
+        if (b[i] != '0' && b[i] != '1')
             return 0;
 
-        result = result * 2 + (*b - '0');
+        result = result * 2 + (b[i] - '0');
+        i++;
     }
 
     return result;
+}
+
+int main() {
+    const char *binary = "101010";
+    unsigned int decimal = binary_to_uint(binary);
+    printf("Decimal: %u\n", decimal);
+
+    return 0;
 }
